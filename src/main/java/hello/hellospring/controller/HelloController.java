84d49443d647edd84dbbb538a90d8b75054cc791
Yunju Jang <rlo3olr@naver.com>
2021-02-11 00:23:41 +0900

    2021/02/11 MVC와 템플릿 엔진 실습 - HelloController 수정 (hello-mvc 추가) - hello-template.html 작성

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller // Controller 임을 알려줌
 public class HelloController {
@@ -11,5 +12,13 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+                            // 외부에서 파라미터를 받을 것이다.
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        // 파라미터로 받아온 name 값을 model에 담는다.
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
