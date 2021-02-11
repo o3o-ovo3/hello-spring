4075552ca7bc75a1b22382916110711ff937da8c
Yunju Jang <rlo3olr@naver.com>
2021-02-11 20:24:53 +0900

    2021/02/11 회원 도메인과 리포지토리 만들기 실습 - Member 도메인 작성 - MemberRepository 작성

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member); // 회원 저장
+    Optional<Member> findById(Long id); // id로 member를 찾기
+    Optional<Member> findById(String name); // name으로 찾기
+    List<Member> findAll(); // 모든 List 반환
+}
---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); // id 셋팅
+        store.put(member.getId(), member); // Map에 저장
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // null이 반환되면 optional로 감쌈
+    }
+
+    @Override
+    public Optional<Member> findById(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name)).findAny();
+        // 루프를 돌아서 Map에서 찾아 하나라도 찾으면 반환, 없으면 null이 Optional로 감싸서 반환됨
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); // store에 있는 memeber들이 반환됨
+    }
+}
