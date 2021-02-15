fb63ab6c7d850fe5422de4770105a2f6916b9feb
Yunju Jang <rlo3olr@naver.com>
2021-02-16 01:26:40 +0900

    2021/02/16 회원 웹 기능 - 조회 실습 - MemberController 수정 (list 메소드 작성) - MemberForm 수정 (파라미터 누락 수정) - memberList.html 작성

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -29,5 +32,12 @@ public class MemberController {
         memberService.join(member);
         
         return "redirect:/"; // 회원가입이 끝나면 홈 화면으로 돌려보냄
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers(); // 회원 목록 전체 조회
+        model.addAttribute("members", members); // 모델에 회원 목록을 넣어준다.
+        return "members/memberList";
     }
 }
